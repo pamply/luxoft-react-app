f0e6fe8723fcda573e44ed69aa54c7350b15d9fa
Manuel Pamplona Rosado <mpamplona@luxoft.com>
2019-11-21 01:09:54 -0600

    Adding login-form component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./styles/index.scss";
-import { Layout } from "./components/layout";
-import { BrowserRouter as Router } from "react-router-dom";
+import "./index.scss";
+import Logo from "./assets/logo.png";
+import { LoginForm } from "./components/login-form.js";
 
+const NavBar = () => (
+  <nav
+    className="navbar navbar-expand-lg navbar"
+    style={{ backgroundColor: "#243387" }}
+  >
+    <a className="navbar-brand" href="#">
+      <img src={Logo} height="50" alt="" />
+    </a>
+  </nav>
+);
 const App = () => (
-  <Router>
-    <Layout />
-  </Router>
+  <>
+    <div className="container-fluid">
+      <NavBar />
+      <div className="content bg-light">
+        <LoginForm />
+      </div>
+    </div>
+  </>
 );
 
 ReactDOM.render(<App />, document.querySelector("#app"));
