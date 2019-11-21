0fb2539bc66dfa29e89d85581fa9c2956045acae
Manuel Pamplona Rosado <mpamplona@luxoft.com>
2019-11-21 00:54:46 -0600

    Adding image loader

--- src/index.js
+++ src/index.js
@@ -2,15 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.scss";
+import Logo from "./assets/logo.png";
 
+const NavBar = () => (
+  <nav
+    className="navbar navbar-expand-lg navbar"
+    style={{ backgroundColor: "#243387" }}
+  >
+    <a className="navbar-brand" href="#">
+      <img src={Logo} height="50" alt="" />
+    </a>
+  </nav>
+);
 const App = () => (
-  <div class="container">
-    <div class="row">
-      <div class="col-sm">One of three columns</div>
-      <div class="col-sm">One of three columns</div>
-      <div class="col-sm">One of three columns</div>
+  <>
+    <div className="container-fluid">
+      <NavBar />
+      <div className="content bg-light">
+        <div class="jumbotron bg-light">
+          <h1 class="display-4">Hello, world!</h1>
+        </div>
+      </div>
     </div>
-  </div>
+  </>
 );
 
 ReactDOM.render(<App />, document.querySelector("#app"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,31 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist")
   },
   module: {
     rules: [
       {
         test: /\.m?jsx?$/,
         exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader'
+        loader: "babel-loader"
       },
       {
         test: /\.(css)|(scss)$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
       }
     ]
   },
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(__dirname, "dist"),
     port: 9000
   }
 };
