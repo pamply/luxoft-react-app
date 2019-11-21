53e9730eed223a24cf0d1065cf96ba833dfed33c
Manuel Pamplona Rosado <mpamplona@luxoft.com>
2019-11-21 01:09:54 -0600

    Adding login-form component

---
+++ src/components/login-form.js
@@ -0,0 +1,30 @@
+import React from "react";
+export const LoginForm = () => (
+  <form>
+    <div className="form-group">
+      <label htmlFor="exampleInputEmail1">Email address</label>
+      <input
+        type="email"
+        className="form-control"
+        id="exampleInputEmail1"
+        aria-describedby="emailHelp"
+        placeholder="Enter email"
+      />
+      <small id="emailHelp" className="form-text text-muted">
+        We'll never share your email with anyone else.
+      </small>
+    </div>
+    <div className="form-group">
+      <label htmlFor="exampleInputPassword1">Password</label>
+      <input
+        type="password"
+        className="form-control"
+        id="exampleInputPassword1"
+        placeholder="Password"
+      />
+    </div>
+    <button type="submit" className="btn btn-primary">
+      Submit
+    </button>
+  </form>
+);
