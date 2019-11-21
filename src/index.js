8bdbbedcb2dcd783b5f471758c3a2db24b772007
Manuel Pamplona Rosado <mpamplona@luxoft.com>
2019-11-20 23:39:15 -0600

    Adding prettier

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.scss';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.scss";
 
 const App = () => (
-    <div className="alert alert-primary" role="alert">Hello world, React Bootcamp</div>
-)
+  <div class="container">
+    <div class="row">
+      <div class="col-sm">One of three columns</div>
+      <div class="col-sm">One of three columns</div>
+      <div class="col-sm">One of three columns</div>
+    </div>
+  </div>
+);
 
 ReactDOM.render(<App />, document.querySelector("#app"));
