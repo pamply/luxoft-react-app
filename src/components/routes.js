37021cab882c6121f8ef022332099f134e9bdbc8
Manuel Pamplona Rosado <mpamplona@luxoft.com>
2019-11-22 04:22:23 -0600

    Adding to the main view when login is success or registration is done

---
+++ src/components/routes.js
@@ -0,0 +1,22 @@
+import { RegisterForm } from "./register-form";
+import { LoginForm } from "./login-form";
+import { Main } from "./main";
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+
+export const Routes = () => (
+  <Switch>
+    <Route children={props => <LoginForm {...props} />} exact path="/" />
+    <Route
+      children={props => <RegisterForm {...props} />}
+      exact
+      path="/register"
+    />
+    <Route
+      children={props => <RegisterForm {...props} />}
+      exact
+      path="/register"
+    />
+    <Route children={props => <Main {...props} />} exact path="/main" />
+  </Switch>
+);
