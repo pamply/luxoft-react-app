d3fdb4114bb3d2174bd6770ea8f3110506301e28
BisaDev <bisa@maniak.com.mx>
2019-11-28 18:27:05 -0600

    Conditionally renders log out link

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,26 +1,42 @@
 import Logo from "../assets/logo.png";
 import React from "react";
-import { Routes } from "./routes";
-const NavBar = () => (
-  <nav
-    className="navbar navbar-expand-lg navbar"
-    style={{ backgroundColor: "#243387" }}
-  >
-    <a className="navbar-brand" href="#">
-      <img src={Logo} height="50" alt="" />
-    </a>
-  </nav>
-);
+import {Routes} from "./routes";
+
+const NavBar = props => {
+    let isLoggedIn;
+
+    try {
+        const {email} = props.history.location.state;
+        isLoggedIn = true
+    } catch (e) {
+        if (e instanceof TypeError) {
+            isLoggedIn = false
+        }
+    }
+
+    return (
+        <nav
+            className="navbar navbar-expand-lg navbar"
+            style={{backgroundColor: "#243387"}}
+        >
+            <a className="navbar-brand" href="#">
+                <img src={Logo} height="50" alt=""/>
+            </a>
+            <a href="/" className={isLoggedIn ? "" : "hidden"}>
+              Log out
+            </a>
+        </nav>
+    );
+};
 
 export const Layout = props => (
-  <>
+
     <div className="container-fluid">
-      <NavBar />
-      <div className="content bg-light">
-        <div className="form-container">
-          <Routes {...props} />
+        <NavBar/>
+        <div className="content bg-light">
+            <div className="form-container">
+                <Routes {...props} />
+            </div>
         </div>
-      </div>
     </div>
-  </>
 );
