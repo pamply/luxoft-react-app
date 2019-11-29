1618fb49ce988e452d1e622ea57fdc27b3c5d8ec
BisaDev <bisa@maniak.com.mx>
2019-11-28 18:27:05 -0600

    Conditionally renders log out link

--- src/components/main.js
+++ src/components/main.js
@@ -1,12 +1,16 @@
 import React from "react";
 
 export const Main = props => {
-  let welcomeMsg = "Please log in or create a new account";
+    let welcomeMsg;
 
-  if(props.location.state) {
-    const { email } = props.history.location.state;
-    welcomeMsg = `Hello ${email}`
-  }
-    
-  return <div>{welcomeMsg}</div>;
+    try {
+        const {email} = props.history.location.state;
+        welcomeMsg = `Hello ${email}`
+    } catch (e) {
+        if (e instanceof TypeError) {
+            welcomeMsg = "Please log in or create a new account"
+        }
+    }
+
+    return <div>{welcomeMsg}</div>;
 };
