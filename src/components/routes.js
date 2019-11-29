990ddde744cf6d1bd3b8751ea12909c94f743be4
BisaDev <bisa@maniak.com.mx>
2019-11-28 22:09:49 -0600

    Adds Reset password route/markup

--- src/components/routes.js
+++ src/components/routes.js
@@ -1,17 +1,19 @@
-import { RegisterForm } from "./register-form";
-import { LoginForm } from "./login-form";
-import { Main } from "./main";
+import {RegisterForm} from "./register-form";
+import {LoginForm} from "./login-form";
+import {Main} from "./main";
 import React from "react";
-import { Switch, Route } from "react-router-dom";
+import {ResetPassword} from "./reset-password";
+import {Switch, Route} from "react-router-dom";
 
 export const Routes = () => (
-  <Switch>
-    <Route children={props => <LoginForm {...props} />} exact path="/" />
-    <Route
-      children={props => <RegisterForm {...props} />}
-      exact
-      path="/register"
-    />
-    <Route children={props => <Main {...props} />} exact path="/main" />
-  </Switch>
+    <Switch>
+        <Route children={props => <LoginForm {...props} />} exact path="/"/>
+        <Route children={props => <Main {...props} />} exact path="/main"/>
+        <Route children={props => <ResetPassword {...props} />} exact path="/reset-password"/>
+        <Route
+            children={props => <RegisterForm {...props} />}
+            exact
+            path="/register"
+        />
+    </Switch>
 );
