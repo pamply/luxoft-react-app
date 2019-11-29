06e135da48208de9e9c3bfcac55a61e26ab761e4
BisaDev <bisa@maniak.com.mx>
2019-11-28 21:18:47 -0600

    Adds style file system/ webpack sass configuration

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./index.scss";
+import "./styles/index.scss";
 import { Layout } from "./components/layout";
 import { BrowserRouter as Router } from "react-router-dom";
 
