7cd6d2f4cfefd4c5418d8014076741a21b6a85f8
BisaDev <bisa@maniak.com.mx>
2019-11-28 23:37:19 -0600

    Adds about component routes/base html

--- src/components/routes.js
+++ src/components/routes.js
@@ -1,6 +1,7 @@
 import {RegisterForm} from "./register-form";
 import {LoginForm} from "./login-form";
 import {Main} from "./main";
+import {About} from "./about";
 import React from "react";
 import {ResetPassword} from "./reset-password";
 import {Switch, Route} from "react-router-dom";
@@ -9,6 +10,7 @@ export const Routes = () => (
     <Switch>
         <Route children={props => <LoginForm {...props} />} exact path="/"/>
         <Route children={props => <Main {...props} />} exact path="/main"/>
+        <Route children={props => <About {...props} />} exact path="/about"/>
         <Route children={props => <ResetPassword {...props} />} exact path="/reset-password"/>
         <Route
             children={props => <RegisterForm {...props} />}
