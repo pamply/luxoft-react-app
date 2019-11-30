eb4b85c1db6d34a6b73dfef09560376f2ab93f56
Leonso <v-lemedina@homeaway.com>
2019-11-30 13:13:45 -0600

    initial state

--- src/components/coins-dashboard/index.js
+++ src/components/coins-dashboard/index.js
@@ -1,46 +1,8 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { compose } from "recompose";
-import { graphql } from "@apollo/react-hoc";
-
-import {
-  ticketsQuery,
-  ordersQuery,
-  placeOrderMutation,
-  removeOrderMutation
-} from "./graphql";
 
 class Coins extends React.Component {
-  handleBuy = ticker => {
-    this.props.placeOrder({
-      variables: {
-        amount: 1,
-        price: ticker.price,
-        exchange: ticker.exchange
-      }
-    });
-  };
-
-  handleRemove = order => {
-    this.props.removeOrder({
-      variables: {
-        orderId: order.id
-      }
-    });
-  };
-
-  handleRefetch = () => {
-    this.props.tickers.refetch();
-  };
-
-  handleNavigate = () => {
-    this.props.history.push("/back");
-  };
-
   render() {
-    const tickers = this.props.tickers.tickers || [];
-    const orders = this.props.orders.orders || [];
-
     return (
       <div>
         <h2>Tickers</h2>
@@ -57,17 +19,15 @@ class Coins extends React.Component {
             </tr>
           </thead>
           <tbody>
-            {tickers.map(ticker => (
-              <tr key={ticker.exchange}>
-                <th scope="row">{ticker.exchange}</th>
-                <td>{ticker.currency}</td>
-                <td>{ticker.volume}</td>
-                <td>{ticker.price}</td>
-                <td>
-                  <button onClick={() => this.handleBuy(ticker)}>Buy</button>
-                </td>
-              </tr>
-            ))}
+            <tr>
+              <th scope="row">exchange</th>
+              <td>currency</td>
+              <td>volume</td>
+              <td>price</td>
+              <td>
+                <button>Buy</button>
+              </td>
+            </tr>
           </tbody>
         </table>
 
@@ -82,18 +42,14 @@ class Coins extends React.Component {
             </tr>
           </thead>
           <tbody>
-            {orders.map(order => (
-              <tr key={order.id}>
-                <th scope="row">{order.exchange}</th>
-                <td>{order.amount}</td>
-                <td>{order.price}</td>
-                <td>
-                  <button onClick={() => this.handleRemove(order)}>
-                    Remove
-                  </button>
-                </td>
-              </tr>
-            ))}
+            <tr>
+              <th scope="row">exchange</th>
+              <td>amount</td>
+              <td>price</td>
+              <td>
+                <button>Remove</button>
+              </td>
+            </tr>
           </tbody>
         </table>
       </div>
@@ -101,43 +57,7 @@ class Coins extends React.Component {
   }
 }
 
-export const CoinsDashboard = compose(
-  graphql(ordersQuery, {
-    name: "orders",
-    options: { fetchPolicy: "cache-first" }
-  }),
-  graphql(ticketsQuery, {
-    name: "tickers",
-    options: { fetchPolicy: "cache-first" }
-  }),
-  graphql(placeOrderMutation, {
-    name: "placeOrder",
-    options: {
-      // refetchQueries: ["getOrders"]
-      update: (proxy, { data: { placeOrder } }) => {
-        const data = proxy.readQuery({ query: ordersQuery });
-        proxy.writeQuery({
-          query: ordersQuery,
-          data: { ...data, orders: [...data.orders, placeOrder] }
-        });
-      }
-    }
-  }),
-  graphql(removeOrderMutation, {
-    name: "removeOrder",
-    options: {
-      // refetchQueries: ["getOrders"]
-      update: (proxy, { data: { removeOrder } }) => {
-        const data = proxy.readQuery({ query: ordersQuery });
-        const orders = data.orders.filter(n => n.id !== removeOrder);
-        proxy.writeQuery({
-          query: ordersQuery,
-          data: { ...data, orders }
-        });
-      }
-    }
-  })
-)(Coins);
+export const CoinsDashboard = Coins;
 
 Coins.propTypes = {
   history: PropTypes.shape({
