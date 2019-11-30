d9ab46bf5cd937d6e4fec748eab95cfe1b9a91ff
BisaDev <bisa@maniak.com.mx>
2019-11-29 23:11:57 -0600

    Rework: simplifies condition/ separates components

--- src/components/reset-password.js
+++ src/components/reset-password.js
@@ -1,47 +1,54 @@
-import React, {Component} from "react"
+import React, { Component } from 'react'
+
+const SuccessPrompt = () => (
+  <div className="alert alert-success" role="alert">Success!</div>
+)
+
+const ResetForm = props => (
+  <form>
+    <div className="reset-pwd-group form-group d-flex flex-column">
+      <label className="reset-label" htmlFor="reset-pwd">Reset your password</label>
+      <input
+        id="reset-pwd"
+        type="password"
+        className="reset-input"
+        placeholder="******"
+        value={props.emailValue}
+        onChange={props.handleChange}
+      />
+      <button onClick={props.onSubmit}
+        className="invalid-feedback btn btn-primary">
+                Go!
+      </button>
+    </div>
+  </form>
+)
 
 export class ResetPassword extends Component {
-    state = { password: "" , display: "form"};
+    state = { password: '', submitted: false };
 
     handleInputChange = e => {
-        const { value: password } = e.target;
-        this.setState({
-            password
-        });
+      const { value: password } = e.target
+      this.setState({
+        password
+      })
     };
 
-    validatePassword = () => {
-        if(this.state.password.length > 2) {
-            this.setState({display: "success"})
-        }
+    validateEntry = () => {
+      if (this.state.password.length > 2) {
+        this.setState({ submitted: true })
+      }
     };
 
-    render() {
-        const {email , display} = this.state;
-        if(display === "form") {
-            return (
-                <form>
-                    <div className="reset-pwd-group form-group d-flex flex-column">
-                        <label className="reset-label" htmlFor="reset-pwd">Reset your password</label>
-                        <input
-                            id="reset-pwd"
-                            type="password"
-                            className="reset-input"
-                            placeholder="******"
-                            value={email}
-                            onChange={this.handleInputChange}
-                        />
-                        <button onClick={this.validatePassword}
-                            className="invalid-feedback btn btn-primary">
-                            Go!
-                        </button>
-                    </div>
-                </form>
-            )
-        } else if (display === "success") {
-            return (
-                <div className="alert alert-success" role="alert">Success!</div>
-            )
-        }
+    render () {
+      const { email, submitted } = this.state
+      const display = !submitted
+        ? <ResetForm
+          handleChange={this.handleInputChange}
+          emailValue={email}
+          onSubmit={this.validateEntry} />
+        : <SuccessPrompt />
+
+      return display
     }
 }
--- src/components/routes.js
+++ src/components/routes.js
@@ -1,21 +1,21 @@
-import {RegisterForm} from "./register-form";
-import {LoginForm} from "./login-form";
-import {Main} from "./main";
-import {About} from "./about";
-import React from "react";
-import {ResetPassword} from "./reset-password";
-import {Switch, Route} from "react-router-dom";
+import { RegisterForm } from './register-form';
+import { LoginForm } from './login-form';
+import { Main } from './main';
+import { About } from './about';
+import React from 'react';
+import { ResetPassword } from './reset-password';
+import { Switch, Route } from 'react-router-dom';
 
 export const Routes = () => (
-    <Switch>
-        <Route children={props => <LoginForm {...props} />} exact path="/"/>
-        <Route children={props => <Main {...props} />} exact path="/main"/>
-        <Route children={props => <About {...props} />} exact path="/about"/>
-        <Route children={props => <ResetPassword {...props} />} exact path="/reset-password"/>
-        <Route
-            children={props => <RegisterForm {...props} />}
-            exact
-            path="/register"
-        />
-    </Switch>
-);
+  <Switch>
+    <Route children={props => <LoginForm {...props} />} exact path="/" />
+    <Route children={props => <Main {...props} />} exact path="/main" />
+    <Route children={props => <About {...props} />} exact path="/about" />
+    <Route children={props => <ResetPassword {...props} />} exact path="/reset-password" />
+    <Route
+      children={props => <RegisterForm {...props} />}
+      exact
+      path="/register"
+    />
+  </Switch>
+)
