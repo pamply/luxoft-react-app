4ff1cd781ff1046da69a4e3eda26edfe18d0fca0
Manuel Pamplona Rosado <mpamplona@luxoft.com>
2019-11-30 01:45:13 -0600

    Formatting

--- src/components/register-form.js
+++ src/components/register-form.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 const listOfPreferences = ['books', 'music', 'movies', 'pets', 'sports']
 
 export class RegisterForm extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     this.fillAgesDropdown = this.fillAgesDropdown.bind(this)
     this.state = {
@@ -71,7 +71,7 @@ export class RegisterForm extends React.Component {
     })
   };
 
-  fillAgesDropdown () {
+  fillAgesDropdown() {
     const options = []
     for (let i = 18; i < 100; i++) {
       options.push(
@@ -125,7 +125,7 @@ export class RegisterForm extends React.Component {
     )
   };
 
-  render () {
+  render() {
     return (
       <>
         <form>
